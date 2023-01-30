a4ce336a81f3ad3052d30e2e3d93fd9bae153f16
Sabbir Hossain <heartedsabbirdhrubo@gmail.com>
2023-01-30 23:47:52 +0600

    commit

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,99 @@
+import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Task } from "../context/TaskContext";
+
+const useAxios = () => {
+  const { data, setData } = useContext(Task);
+  const navigate = useNavigate()
+
+  const axiosConfig = () => {
+    axios.defaults.baseURL = "http://localhost:7000";
+  };
+
+  const getSectors = async () => {
+    try {
+      axiosConfig();
+      let response = await axios.get("/task/getsectors")
+      setData((prev) => {
+        return {
+          ...prev,
+          sectorOptions: response.data.data,
+        };
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addSector = async (title) => {
+    try {
+      axiosConfig()
+      await axios.post("/task/addsector", {title: title});
+      getSectors();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Get All Task
+  const getTasks = async () => {
+    try {
+      axiosConfig();
+      let response = await axios.get("/task/gettasks");
+      setData((prev) => {
+        return {
+          ...prev,
+          tasks: response.data.data,
+        };
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+
+  // Create New Task
+  const createTask = async (data) => {
+    try {
+      axiosConfig();
+      await axios.post("/task/createtask", {...data}).then( res => {
+        if(res.status === 201){
+          navigate('/edit/' + res.data.data._id)
+        }
+      })
+      getTasks()
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+   // Create New Task
+   const editTask = async (id, data) => {
+
+    try {
+      axiosConfig();
+      await axios.post("/task/edittask/" + id, {...data}).then( res => {
+
+        if(res.status === 200){
+          navigate('/')
+        }
+      })
+      getTasks()
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  return {
+    axiosConfig,
+    getSectors,
+    addSector,
+    getTasks,
+    createTask,
+    editTask
+  };
+};
+
+export default useAxios;
---
+++ src/page/AddEditTasks.jsx
@@ -0,0 +1,118 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import { Task } from "../context/TaskContext";
+import useAxios from './../hooks/useAxios';
+
+const AddTasks = () => {
+  const { getSectors, editTask, axiosConfig} = useAxios();
+  const { data } = useContext(Task);
+  const { id } = useParams()
+  const [input, setInput] = useState({})
+  const [checked, setChecked] = useState(false);
+  
+
+  useEffect(() => {
+    getSectors()
+    // getSingleTask(id)
+    const singleTask = async (id) => {
+       axiosConfig()
+       await axios.get('/task/gettask/' + id).then(res => {
+        setInput((prev) => ({
+           ...prev,
+           ...res.data.data
+        }))     
+       })
+    }
+
+    singleTask(id)
+
+  },[id])
+ 
+
+  // Handle Input
+  const handleInput = (e) => {
+     let name = e.target.name 
+     let value = e.target.value
+     setInput((prev) => ({
+      ...prev,
+      [name] : value
+   }))
+   
+  }
+ 
+  //  Handle Terms 
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+   
+  // Handle Submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!input.name || !input.sector || checked === false) {
+      alert('All Feild Are Required!')
+    } else {
+     let data = {...input, agreeWithTerms:checked}
+     editTask(id, data) 
+   }
+  }
+
+
+ 
+  
+ 
+  return (
+    <div className="conteiner">
+         <div className="row my-5">
+             <div className="col-2"></div>
+             <div className="col-8">
+             <div className="card border p-5">
+
+           <form onSubmit={handleSubmit}>
+       
+          <div className="form-outline mb-4">
+            <span>Name:</span>
+            <input name="name" onChange={handleInput} type="text" value={input.name} className="form-control" />
+          </div>
+
+          <div className="form-outline mb-4">
+            <span>Sector:</span>
+           <select name="sector" onChange={handleInput} className="select w-100 border p-2">
+              {
+                data.sectorOptions && data.sectorOptions.map((item,i) => 
+                <>
+                  <option selected={input.sector === item.title} key={i} value={item.title}>{item.title}</option>
+                </>)
+              }
+           
+            </select>
+          </div>
+
+   
+          <div className="row mb-4">
+            <div className="col">
+        
+              <div className="form-check">
+                <input
+                  type="checkbox"
+                  checked={checked}
+                  onChange={handleChange}
+                />
+                <label className="form-check-label"> Agree with All Terms & Conditions.</label>
+              </div>
+            </div>
+          </div>
+
+   
+          <button type="submit" className="btn btn-primary btn-block">Save</button>
+         </form>
+             </div>    
+             </div>
+             <div className="col-2"></div>
+         </div>
+    </div>
+  )
+};
+
+export default AddTasks;
